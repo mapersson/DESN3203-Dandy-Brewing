29c085179e0f6f545a9dcf0d20bf3463ee818a5d
mapersson <62900241+mapersson@users.noreply.github.com>
2020-11-26 07:42:24 -0700

    Update jquery.js
    Added logic to assign "current" class to the nav link for the currently displayed page.

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -5,6 +5,14 @@ $(function () {
     currentLoc = location.pathname.split("/").slice(-1)[0];
     alert(currentLoc);
 
-    var list = document.getElementById("header-background");
-    var items = list.getElementsByTagName("a");
-})
+    setTimeout(function () {
+        var list = document.getElementById("header-background");
+        var items = list.getElementsByTagName("a");
+        $(items).each(function () {
+            if ($(this).attr("href") == currentLoc) {
+                $(this).addClass("current");
+            }
+        });
+    }, 300);
+
+});
